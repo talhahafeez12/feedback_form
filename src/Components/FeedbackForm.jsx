359753f60ee37ed8c2447cc89e56511156a3cfee
Talha Hafeez <talhahafeez@live.ca>
2024-12-27 17:15:34 -0500

    Completed practice task of the lab

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -6,7 +6,8 @@ const FeedbackForm = () => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
-        feedback: ''
+        feedback: '',
+        rating: ''
     });
 
     const handleChange = (event) => {
@@ -23,6 +24,7 @@ const FeedbackForm = () => {
         Name: ${formData.name}
         Email: ${formData.email}
         Feedback: ${formData.feedback}
+        Rating: ${formData.rating}
         `;
         const isConfirmed = window.confirm(`Please confirm your details:\n\n${confirmationMessage}`);
         if (isConfirmed) {
@@ -30,7 +32,8 @@ const FeedbackForm = () => {
             setFormData({
                 name: '',
                 email: '',
-                feedback: ''
+                feedback: '',
+                rating: ''
             });
             alert('Thank you for your valuable feedback');
         }
@@ -48,6 +51,14 @@ const FeedbackForm = () => {
         <input type="text" name="name" placeholder='Your Name' value={formData.name} onChange={handleChange}/>
         <input type="email" name="email" placeholder='Your Email' value={formData.email} onChange={handleChange}/>
         <textarea name='feedback' placeholder='Your Feedback' value={formData.feedback} onChange={handleChange}></textarea>
+        <div style={{display:'flex',gap:'10px',flexDirection:'column'}}>
+            <span>Rate Us:</span>
+            <p><input type="radio" name="rating" value="1"onChange={handleChange}/> 1</p>
+            <p><input type="radio" name="rating" value="2"onChange={handleChange}/> 2</p>
+            <p><input type="radio" name="rating" value="3"onChange={handleChange}/> 3</p>
+            <p><input type="radio" name="rating" value="4"onChange={handleChange}/> 4</p>
+            <p><input type="radio" name="rating" value="5"onChange={handleChange}/> 5</p>
+        </div>
         <button type='submit'>Submit Feedback</button>
       </form>
     </>
